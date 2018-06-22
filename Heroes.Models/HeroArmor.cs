e05bf8f98ef20759e0b8e09a9fac360e054b413b
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-06-21 19:52:33 -0500

    Added hero armor

---
+++ Heroes.Models/HeroArmor.cs
@@ -0,0 +1,15 @@
+﻿namespace Heroes.Models
+{
+    public class HeroArmor
+    {
+        /// <summary>
+        /// Gets or sets the physical (basic) armor.
+        /// </summary>
+        public int PhysicalArmor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Spell (ability) armor.
+        /// </summary>
+        public int SpellArmor { get; set; }
+    }
+}
