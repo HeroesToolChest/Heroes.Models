7cc35ce559a42c948afa51b63f13e6909e369a61
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-08-06 16:32:48 -0500

    Removed life percentage

--- Heroes.Models/AbilityTalents/Tooltip/TooltipLife.cs
+++ Heroes.Models/AbilityTalents/Tooltip/TooltipLife.cs
@@ -7,11 +7,6 @@
         /// </summary>
         public string LifeCostText { get; set; }
 
-        /// <summary>
-        /// Gets or sets if whether the life is a percentage cost.
-        /// </summary>
-        public bool IsLifePercentage { get; set; } = false;
-
         public override string ToString()
         {
             if (string.IsNullOrEmpty(LifeCostText))
