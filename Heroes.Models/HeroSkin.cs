17dbc2f3bd3ec43158d97473c81b6d716766d0f7
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-01-12 21:39:34 -0600

    Added heroskins class

---
+++ Heroes.Models/HeroSkin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Heroes.Models
+{
+    public class HeroSkin : IName
+    {
+        /// Gets or sets the short name.
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort name used for sorting the hero skins.
+        /// </summary>
+        public string SortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the information text.
+        /// </summary>
+        public string InfoText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search text. Space delimited.
+        /// </summary>
+        public string SearchText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date of the skin.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
