fa75554bda63af6c3714921d3d7869df760ac619
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-02-17 11:30:39 -0600

    Added description type enum

---
+++ Heroes.Models/DescriptionType.cs
@@ -0,0 +1,13 @@
+﻿namespace Heroes.Models
+{
+    public enum DescriptionType
+    {
+        RawDescription = 0,
+        PlainText = 1,
+        PlainTextWithNewlines = 2,
+        PlainTextWithScaling = 3,
+        PlainTextWithScalingWithNewlines = 4,
+        ColoredText = 5,
+        ColoredTextWithScaling = 6,
+    }
+}
