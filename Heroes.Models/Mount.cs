3041976cbef7d03687f5e744d90a71da1a9e57da
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-02-25 17:58:53 -0600

    Added mount class

---
+++ Heroes.Models/Mount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Heroes.Models
+{
+    public class Mount
+    {
+        /// <summary>
+        /// Gets or sets the mount id.
+        /// </summary>
+        public string MountId { get; set; }
+
+        ///
+        /// Gets or sets the short name.
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort name used for sorting the mounts.
+        /// </summary>
+        public string SortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description text.
+        /// </summary>
+        public TooltipDescription Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search text. Space delimited.
+        /// </summary>
+        public string SearchText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attribute id.
+        /// </summary>
+        public string AttributeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date of the mount.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rarity.
+        /// </summary>
+        public Rarity Rarity { get; set; }
+    }
+}
