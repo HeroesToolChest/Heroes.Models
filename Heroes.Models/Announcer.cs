a98062989190ba6e8c3afc00c0c902a88e0c9bbb
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-03-03 00:46:33 -0600

    Added announcer

---
+++ Heroes.Models/Announcer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Heroes.Models
+{
+    public class Announcer : IExtractable
+    {
+        /// <summary>
+        /// Gets or sets the unique id.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hyperlink id.
+        /// </summary>
+        public string HyperlinkId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort name used for sorting the announcers.
+        /// </summary>
+        public string SortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description text.
+        /// </summary>
+        public TooltipDescription Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attribute id.
+        /// </summary>
+        public string AttributeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date of the announcer.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rarity.
+        /// </summary>
+        public Rarity Rarity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of collection category.
+        /// </summary>
+        public string CollectionCategory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image file name.
+        /// </summary>
+        public string ImageFileName { get; set; }
+    }
+}
