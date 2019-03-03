1ac43a3c05a88a94fed769f5ac73f2f5bf2ed33b
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-03-02 19:29:35 -0600

    Added banner and spray

---
+++ Heroes.Models/Banner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Heroes.Models
+{
+    public class Banner : IExtractable
+    {
+        /// <summary>
+        /// Gets or sets the unique id.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hyperlink id.
+        /// </summary>
+        public string HyperlinkId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort name used for sorting the hero banners.
+        /// </summary>
+        public string SortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description text.
+        /// </summary>
+        public TooltipDescription Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attribute id.
+        /// </summary>
+        public string AttributeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date of the banner.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rarity.
+        /// </summary>
+        public Rarity Rarity { get; set; }
+    }
+}
