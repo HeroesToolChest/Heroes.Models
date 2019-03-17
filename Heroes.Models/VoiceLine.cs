5747ce74e3f7e985aadd476089c94b2d08031ecd
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-03-16 19:20:51 -0500

    Added voiceline

---
+++ Heroes.Models/VoiceLine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Heroes.Models
+{
+    public class VoiceLine : IExtractable
+    {
+        /// <summary>
+        /// Gets or sets the unique id.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hyperlink id.
+        /// </summary>
+        public string HyperlinkId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort name used for sorting the voice lines.
+        /// </summary>
+        public string SortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description text.
+        /// </summary>
+        public TooltipDescription Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attribute id.
+        /// </summary>
+        public string AttributeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image file name.
+        /// </summary>
+        public string ImageFileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hero id associated with the voiceline.
+        /// </summary>
+        public string HeroId { get; set; }
+    }
+}
