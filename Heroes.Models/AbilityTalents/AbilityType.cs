026ec89e863a5b62d7eb21abd5b12049f632a203
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-04-24 18:44:38 -0500

    Added flags to abilitytype

--- Heroes.Models/AbilityTalents/AbilityType.cs
+++ Heroes.Models/AbilityTalents/AbilityType.cs
@@ -1,26 +1,29 @@
-﻿namespace Heroes.Models.AbilityTalents
+﻿using System;
+
+namespace Heroes.Models.AbilityTalents
 {
+    [Flags]
     public enum AbilityType
     {
-        Q,
-        W,
-        E,
-        Heroic,
-        Z,
-        Trait,
-        Active,
-        Passive,
-        B,
-        Attack,
-        Stop,
-        Hold,
-        Cancel,
-        Interact,
-        Taunt,
-        Dance,
-        Spray,
-        Voice,
-        ForceMove,
+        Q = 0,
+        W = 1 << 0,
+        E = 1 << 1,
+        Heroic = 1 << 2,
+        Z = 1 << 3,
+        Trait = 1 << 4,
+        Active = 1 << 5,
+        Passive = 1 << 6,
+        B = 1 << 7,
+        Attack = 1 << 8,
+        Stop = 1 << 9,
+        Hold = 1 << 10,
+        Cancel = 1 << 11,
+        Interact = 1 << 12,
+        Taunt = 1 << 13,
+        Dance = 1 << 14,
+        Spray = 1 << 15,
+        Voice = 1 << 16,
+        ForceMove = 1 << 17,
 
         Standard = Q | W | E | Heroic | Z | Trait | Active | Passive | B,
         Misc = Attack | Stop | Stop | Hold | Cancel | Interact | Taunt | Dance | Spray | Voice | ForceMove,
