e82b7cb32c371295036bd5cdc64f6283cdf9eb20
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-05-19 15:45:34 -0500

    Added methods for emoticon

--- Heroes.Models/Emoticon.cs
+++ Heroes.Models/Emoticon.cs
@@ -4,25 +4,29 @@ namespace Heroes.Models
 {
     public class Emoticon : ExtractableBase<Emoticon>, IExtractable
     {
+        private readonly HashSet<string> UniversalAliaseList = new HashSet<string>();
+        private readonly HashSet<string> LocalizedAliaseList = new HashSet<string>();
+        private readonly HashSet<string> SearchTextList = new HashSet<string>();
+
         /// <summary>
         /// Gets or sets the description text.
         /// </summary>
         public TooltipDescription Description { get; set; }
 
         /// <summary>
-        /// Gets or sets a collection of universal aliases for the emoticon.
+        /// Gets a collection of universal aliases for the emoticon.
         /// </summary>
-        public ICollection<string> UniversalAliases { get; set; } = new List<string>();
+        public IEnumerable<string> UniversalAliases => UniversalAliaseList;
 
         /// <summary>
-        /// Gets or sets a collection of localized aliases for the emoticon.
+        /// Gets a collection of localized aliases for the emoticon.
         /// </summary>
-        public ICollection<string> LocalizedAliases { get; set; } = new List<string>();
+        public IEnumerable<string> LocalizedAliases => LocalizedAliaseList;
 
         /// <summary>
-        /// Gets or sets a collection of search texts for the emoticon.
+        /// Gets a collection of search texts for the emoticon.
         /// </summary>
-        public ICollection<string> SearchTexts { get; set; } = new List<string>();
+        public IEnumerable<string> SearchTexts => SearchTextList;
 
         /// <summary>
         /// Gets or sets if the aliases are case sensitive.
@@ -48,5 +52,62 @@ namespace Heroes.Models
         /// Gets or sets the image properties of the emoticon.
         /// </summary>
         public EmoticonImage Image { get; set; } = new EmoticonImage();
+
+        /// <summary>
+        /// Adds a universal aliase value. Replaces if value already exists in collection.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddUniversalAliase(string value)
+        {
+            UniversalAliaseList.Add(value);
+        }
+
+        /// <summary>
+        /// Determines whether the value exists.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool UniversalAliaseExists(string value)
+        {
+            return UniversalAliaseList.Contains(value);
+        }
+
+        /// <summary>
+        /// Adds a localized aliase value. Replaces if value already exists in collection.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddLocalizedAliase(string value)
+        {
+            LocalizedAliaseList.Add(value);
+        }
+
+        /// <summary>
+        /// Determines whether the value exists.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool LocalizedAliaseExists(string value)
+        {
+            return LocalizedAliaseList.Contains(value);
+        }
+
+        /// <summary>
+        /// Adds a search text value. Replaces if value already exists in collection.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddSearchText(string value)
+        {
+            SearchTextList.Add(value);
+        }
+
+        /// <summary>
+        /// Determines whether the value exists.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool SearchTextExists(string value)
+        {
+            return SearchTextList.Contains(value);
+        }
     }
 }
