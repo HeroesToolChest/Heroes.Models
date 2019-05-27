76a67959f1b52336002c555d047afb1ac319b153
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-05-27 17:24:53 -0500

    Updated abilitytier flags

--- Heroes.Models/AbilityTalents/AbilityTier.cs
+++ Heroes.Models/AbilityTalents/AbilityTier.cs
@@ -7,32 +7,36 @@ namespace Heroes.Models.AbilityTalents
     public enum AbilityTier
     {
         [Description("Unknown Ability")]
-        Unknown,
+        Unknown = 0,
         [Description("Basic Ability")]
-        Basic,
+        Basic = 1 << 0,
         [Description("Heroic Ability")]
-        Heroic,
+        Heroic = 1 << 1,
         [Description("Trait Ability")]
-        Trait,
+        Trait = 1 << 2,
         [Description("Mount Ability")]
-        Mount,
+        Mount = 1 << 3,
         [Description("Activable Ability")]
-        Activable,
+        Activable = 1 << 4,
         [Description("Hearth Ability")]
-        Hearth,
+        Hearth = 1 << 5,
         [Description("Taunt Ability")]
-        Taunt,
+        Taunt = 1 << 6,
         [Description("Dance Ability")]
-        Dance,
+        Dance = 1 << 7,
         [Description("Spray Ability")]
-        Spray,
+        Spray = 1 << 8,
         [Description("Voice Ability")]
-        Voice,
+        Voice = 1 << 9,
         [Description("Map Mechanic Ability")]
-        MapMechanic,
+        MapMechanic = 1 << 10,
         [Description("Interact Ability")]
-        Interact,
+        Interact = 1 << 11,
         [Description("Action Ability")]
-        Action,
+        Action = 1 << 12,
+
+        Standard = Basic | Heroic | Trait | Mount | Activable,
+        Special = Hearth | Taunt | Dance | Spray | Voice | MapMechanic,
+        Misc = Interact | Activable,
     }
 }
