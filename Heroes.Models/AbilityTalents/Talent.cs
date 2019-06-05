28ee31803b26853acb9847d9843047c66b08409a
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-06-05 18:31:03 -0500

    Added abilityTalentLinkIds back

--- Heroes.Models/AbilityTalents/Talent.cs
+++ Heroes.Models/AbilityTalents/Talent.cs
@@ -1,7 +1,11 @@
-﻿namespace Heroes.Models.AbilityTalents
+﻿using System.Collections.Generic;
+
+namespace Heroes.Models.AbilityTalents
 {
     public class Talent : AbilityTalentBase
     {
+        private readonly HashSet<string> AbilityTalentLinkIdList = new HashSet<string>();
+
         public Talent() { }
 
         public Talent(AbilityTalentBase talentBase)
@@ -23,6 +27,45 @@
         /// Gets or sets the column number, also known as the sort index number.
         /// </summary>
         public int Column { get; set; }
+
+        /// <summary>
+        /// Gets the ability and talents that the talent affects or upgrades.
+        /// </summary>
+        public IEnumerable<string> AbilityTalentLinkIds => AbilityTalentLinkIdList;
+
+        /// <summary>
+        /// Gets the amount of abilityTalentLinkIds.
+        /// </summary>
+        public int AbilityTalentLinkIdsCount => AbilityTalentLinkIdList.Count;
+
+        /// <summary>
+        /// Adds an abilityTalentLinkId.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddAbilityTalentLinkId(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new System.ArgumentException("Argument cannot be null or empty.", nameof(value));
+            }
+
+            AbilityTalentLinkIdList.Add(value);
+        }
+
+        /// <summary>
+        /// Determines whether the value exists.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool ContainsAbilityTalentLinkId(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new System.ArgumentException("Argument cannot be null or empty.", nameof(value));
+            }
+
+            return AbilityTalentLinkIdList.Contains(value);
+        }
 
         public override string ToString() => $"{Tier.GetFriendlyName()} | {ReferenceNameId}";
     }
