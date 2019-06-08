5f56fd292fcc4812d902c78ce70e0d8adf73534f
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-06-08 18:03:43 -0500

    Updated check

--- Heroes.Models/Unit.cs
+++ Heroes.Models/Unit.cs
@@ -383,7 +383,7 @@ namespace Heroes.Models
             if (!AbilitiesByName.ContainsKey(ability.DuplicateId))
             {
                 AbilitiesByName.Add(ability.DuplicateId, ability);
-                AbilitiesById.Add(ability.ReferenceNameId, ability);
+                AbilitiesById[ability.ReferenceNameId] = ability;
             }
         }
 
