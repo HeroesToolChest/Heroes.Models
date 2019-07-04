03fdcaf97dab5fe2d6ac661f587992beae20280d
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-07-04 16:46:25 -0500

    Added more portrait file properties

--- Heroes.Models/HeroPortrait.cs
+++ Heroes.Models/HeroPortrait.cs
@@ -26,5 +26,20 @@
         /// Gets or sets the target portrait file name.
         /// </summary>
         public string TargetPortraitFileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the party frame file name.
+        /// </summary>
+        public string PartyFrameFileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the draft screen file name.
+        /// </summary>
+        public string DraftScreenFileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimap icon file name.
+        /// </summary>
+        public string MiniMapIconFileName { get; set; }
     }
 }
