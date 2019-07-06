d090daf07c36e96c84ad8ab397711af5a8262f02
Kevin Oliva <kevin.k.oliva@gmail.com>
2019-07-06 11:25:23 -0500

    Added target info panel

--- Heroes.Models/UnitPortrait.cs
+++ Heroes.Models/UnitPortrait.cs
@@ -8,5 +8,10 @@ namespace Heroes.Models
         /// Gets or sets the party frame file name.
         /// </summary>
         public ICollection<string> PartyFrameFileName { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Gets or sets the target info panel file name.
+        /// </summary>
+        public string TargetInfoPanelFileName { get; set; }
     }
 }
