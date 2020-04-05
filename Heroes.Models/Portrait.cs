f27c3e88042e9731c6b1f2175f2c96143e43e1dd
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-04-05 18:33:14 -0500

    Added new properties

--- Heroes.Models/Portrait.cs
+++ Heroes.Models/Portrait.cs
@@ -14,5 +14,25 @@
         /// Gets or sets the event name associated with this portrait.
         /// </summary>
         public string? EventName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description text.
+        /// </summary>
+        public TooltipDescription? Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of collection category.
+        /// </summary>
+        public string? CollectionCategory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image file name.
+        /// </summary>
+        public string ImageFileName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the icon slot number.
+        /// </summary>
+        public int IconSlot { get; set; } = 0;
     }
 }
