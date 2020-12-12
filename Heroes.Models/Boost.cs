5c69f81b12c4800d2dd71f4f6ec4a6eea701d6c2
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-12-11 19:06:01 -0600

    Added boost data

---
+++ Heroes.Models/Boost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Heroes.Models
+{
+    /// <summary>
+    /// Contains the information for boost data.
+    /// </summary>
+    public class Boost : ExtractableBase<Boost>, IExtractable
+    {
+        /// <summary>
+        /// Gets or sets the sort name used for sorting the boosts.
+        /// </summary>
+        public string? SortName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the release date of the boost.
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
