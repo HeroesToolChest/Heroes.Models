4afb9f200590b213ccfbc5f48c85f9149ef29299
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-12-19 21:19:34 -0600

    Added TypeDescription

---
+++ Heroes.Models/TypeDescription.cs
@@ -0,0 +1,18 @@
+﻿namespace Heroes.Models
+{
+    /// <summary>
+    /// Contains the information for type description data.
+    /// </summary>
+    public class TypeDescription : ExtractableBase<Boost>, IExtractable
+    {
+        /// <summary>
+        /// Gets or sets the texture sheet associated with the type description.
+        /// </summary>
+        public TextureSheet TextureSheet { get; set; } = new TextureSheet();
+
+        /// <summary>
+        /// Gets or sets the index of the icon on the textsheet.
+        /// </summary>
+        public int IconIndex { get; set; }
+    }
+}
