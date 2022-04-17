f926738a2d7df71c3c1e7450c1ba481e0076e67d
Kevin Oliva <kevin.k.oliva@gmail.com>
2022-04-17 00:54:49 -0500

    Added benchmarking project

---
+++ Heroes.Models.Benchmarks/DescriptionValidatorBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿namespace Heroes.Models.Benchmarks;
+
+[MemoryDiagnoser]
+public class DescriptionValidatorBenchmarks
+{
+    private readonly string _dvaMechSelfDestruct = "Eject from the Mech, setting it to self-destruct after <c val=\"#TooltipNumbers\">4</c> seconds. Deals <c val=\"#TooltipNumbers\">400</c> to <c val=\"#TooltipNumbers\">1200</c> damage in a large area, depending on distance from center. Only deals <c val=\"#TooltipNumbers\">50%</c> damage against Structures.</n></n><c val=\"FF8000\">Gain <c val=\"#TooltipNumbers\">1%</c> Charge for every <c val=\"#TooltipNumbers\">2</c> seconds spent Basic Attacking, and <c val=\"#TooltipNumbers\">30%</c> Charge per <c val=\"#TooltipNumbers\">100%</c> of Mech Health lost.</c>";
+
+    public DescriptionValidatorBenchmarks()
+    {
+    }
+
+    [Benchmark]
+    public void Validate100()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            DescriptionValidator.Validate(_dvaMechSelfDestruct);
+        }
+    }
+
+    [Benchmark]
+    public void Validate1000()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            DescriptionValidator.Validate(_dvaMechSelfDestruct);
+        }
+    }
+}
